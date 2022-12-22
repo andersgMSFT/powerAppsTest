e4c43438f85dc6035347a7571f0afeac9bc0287b
Anders Nørskov <andersg@microsoft.com>
2022-12-22 12:31:54 +0100

    fix workflow

--- script/commitGitChanges.ps1
+++ script/commitGitChanges.ps1
@@ -7,9 +7,13 @@ param(
 )
 
 if (git status --porcelain) {
+  Write-Host "Commiting changes to branch";
   git config user.email "git@workflow.com"; 
   git config user.name "Pull changes workflow"; 
   git add $solutionName; 
   git commit -m "pull latest solution changes from " + $environmentName; 
   git push; 
+}
+else {
+  Write-Host "No changes to commit";
 }
