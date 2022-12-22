7576971356e9916f4e21b444ac62accf3891b200
andersgMSFT <90765077+andersgMSFT@users.noreply.github.com>
2022-12-22 12:17:26 +0100

    Update commitGitChanges.ps1

--- script/commitGitChanges.ps1
+++ script/commitGitChanges.ps1
@@ -1,1 +1,14 @@
-test
+[CmdletBinding()]
+param(
+    [Parameter(Position = 0, mandatory = $true)]
+    [string] $solutionName,
+)
+
+if(git status --porcelain)
+{
+  git config user.email "git@workflow.com"; 
+  git config user.name "Workflow"; 
+  git add $solutionName; 
+  git commit -m "pull latest solution changes"; 
+  git push; 
+}
