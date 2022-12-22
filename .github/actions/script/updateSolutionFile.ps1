44c4706af18309186ea9f3e2a86d3c0cba0c6841
Anders Nørskov <andersg@microsoft.com>
2022-12-22 20:41:20 +0100

    Update workflow scripts

--- .github/actions/script/updateSolutionFile.ps1
+++ .github/actions/script/updateSolutionFile.ps1
@@ -2,10 +2,10 @@
 param(
     [Parameter(Position = 0, mandatory = $true)]
     [string] $solutionPath,
-    [Parameter(Position = 0, mandatory = $false)]
+    [Parameter(Position = 1, mandatory = $false)]
     [string] $version,
-    [Parameter(Position = 0, mandatory = $false)]
-    [string] $dev
+    [Parameter(Position = 2, mandatory = $false)]
+    [string] $postFix
 )
 
 $xmlFile = (Get-ChildItem $solutionPath);
@@ -19,16 +19,17 @@ if ($xmlFile.Exists) {
         $node.'#text' = $version;
     }
 
-    if ($dev -eq "true") {
+    if ($postFix) {
         $nodeWithName = $xml.SelectSingleNode("//UniqueName");
-        $newName = $nodeWithName.'#text' + "_dev"
+        $newName = $nodeWithName.'#text' + "__" + $postFix;
         Write-Host "Updating solution name: "$newName;
         $nodeWithName.'#text' = $newName;    
     }
     else {
         $nodeWithName = $xml.SelectSingleNode("//UniqueName");
-        if ($nodeWithName.'#text' -match "_dev") {
-            $newName = $nodeWithName.'#text'.subString($nodeWithName.'#text'.Length - 4)
+        if ($nodeWithName.'#text' -match "__") {
+            $postFixIndex = $nodeWithName.'#text'.LastIndexOf("__");
+            $newName = $nodeWithName.'#text'.subString($postFixIndex)
             Write-Host "Updating solution name: "$newName;
             $nodeWithName.'#text' = $newName;    
         }
