6c5bb03dea9e8a780c968bfd2f49050d0d458b50
Anders Nørskov <andersg@microsoft.com>
2022-12-22 12:25:27 +0100

    fix workflow

--- script/commitGitChanges.ps1
+++ script/commitGitChanges.ps1
@@ -1,14 +1,15 @@
 [CmdletBinding()]
 param(
-    [Parameter(Position = 0, mandatory = $true)]
-    [string] $solutionName,
+  [Parameter(Position = 0, mandatory = $true)]
+  [string] $solutionName,
+  [Parameter(Position = 1, mandatory = $true)]
+  [string] $environmentName
 )
 
-if(git status --porcelain)
-{
+if (git status --porcelain) {
   git config user.email "git@workflow.com"; 
-  git config user.name "Workflow"; 
+  git config user.name "Pull changes workflow"; 
   git add $solutionName; 
-  git commit -m "pull latest solution changes"; 
+  git commit -m "pull latest solution changes from " + $environmentName; 
   git push; 
 }
