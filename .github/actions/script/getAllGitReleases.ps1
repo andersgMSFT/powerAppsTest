b5e7c28615a388a7bcf9e544e1db2bd8fb32e54c
Anders Nørskov <agnoerskov@outlook.com>
2023-01-11 09:29:09 +0100

    Update deploy from Releases script

--- .github/actions/script/getAllGitReleases.ps1
+++ .github/actions/script/getAllGitReleases.ps1
@@ -17,6 +17,102 @@ function GetHeader {
     return $headers
 }
 
+function InvokeWebRequest {
+    Param(
+        [Hashtable] $headers,
+        [string] $method,
+        [string] $body,
+        [string] $outFile,
+        [string] $uri,
+        [switch] $retry,
+        [switch] $ignoreErrors
+    )
+
+    try {
+        $params = @{ "UseBasicParsing" = $true }
+        if ($headers) {
+            $params += @{ "headers" = $headers }
+        }
+        if ($method) {
+            $params += @{ "method" = $method }
+        }
+        if ($body) {
+            $params += @{ "body" = $body }
+        }
+        if ($outfile) {
+            $params += @{ "outfile" = $outfile }
+        }
+        Invoke-WebRequest  @params -Uri $uri
+    }
+    catch {
+        $message = GetExtendedErrorMessage -errorRecord $_
+        if ($retry) {
+            Write-Host $message
+            Write-Host "...retrying in 1 minute"
+            Start-Sleep -Seconds 60
+            try {
+                Invoke-WebRequest  @params -Uri $uri
+                return
+            }
+            catch {}
+        }
+        if ($ignoreErrors.IsPresent) {
+            Write-Host $message
+        }
+        else {
+            Write-Host "::Error::$message"
+            throw $message
+        }
+    }
+}
+
+function GetExtendedErrorMessage {
+    Param(
+        $errorRecord
+    )
+
+    $exception = $errorRecord.Exception
+    $message = $exception.Message
+
+    try {
+        $errorDetails = $errorRecord.ErrorDetails | ConvertFrom-Json
+        $message += " $($errorDetails.error)`r`n$($errorDetails.error_description)"
+    }
+    catch {}
+    try {
+        if ($exception -is [System.Management.Automation.MethodInvocationException]) {
+            $exception = $exception.InnerException
+        }
+        $webException = [System.Net.WebException]$exception
+        $webResponse = $webException.Response
+        try {
+            if ($webResponse.StatusDescription) {
+                $message += "`r`n$($webResponse.StatusDescription)"
+            }
+        }
+        catch {}
+        $reqstream = $webResponse.GetResponseStream()
+        $sr = new-object System.IO.StreamReader $reqstream
+        $result = $sr.ReadToEnd()
+        try {
+            $json = $result | ConvertFrom-Json
+            $message += "`r`n$($json.Message)"
+        }
+        catch {
+            $message += "`r`n$result"
+        }
+        try {
+            $correlationX = $webResponse.GetResponseHeader('ms-correlation-x')
+            if ($correlationX) {
+                $message += " (ms-correlation-x = $correlationX)"
+            }
+        }
+        catch {}
+    }
+    catch {}
+    $message
+}
+
 Write-Host "Analyzing releases $api_url/repos/$repository/releases"
 $releases = @(InvokeWebRequest -Headers (GetHeader -token $token) -Uri "$api_url/repos/$repository/releases" | ConvertFrom-Json)
 if ($releases.Count -gt 1) {
